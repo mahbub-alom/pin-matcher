2babe9195d7a9b1a1514fe1d0cb6462f574042c7
mahbub <mdmahbuba034@gmail.com>
2023-02-17 01:56:24 +0600

    second commit

--- js files/apps.js
+++ js files/apps.js
@@ -8,12 +8,52 @@ function getPin(){
         return getPin();
     }
 }
-
 function pinGenerator(){
     const random = Math.round(Math.random()*10000);
     return random;
 }
+
+
 document.getElementById('generate-btn').addEventListener('click', function(){
     const pin = getPin();
-    console.log(pin);
+    const displayPin = document.getElementById('form-control');
+    displayPin.value = pin;
+})
+document.getElementById('calculator').addEventListener('click', function(event){
+    const getCalculatorNumber = event.target.innerText;
+    const fromControl = document.getElementById('from-control');
+    const previousTypeNumber = fromControl.value;
+    if(isNaN(getCalculatorNumber)){
+        if(getCalculatorNumber === 'C'){
+            fromControl.value = '';
+        }
+        else if(getCalculatorNumber === '<'){
+            const digits = previousTypeNumber.split('');
+            digits.pop();
+            const lastDigits = digits.join('');
+            fromControl.value = lastDigits;
+        }
+    }
+    else{
+        const newTypeNumber = previousTypeNumber + getCalculatorNumber;
+        fromControl.value = newTypeNumber;
+    }
+})
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+    const generatePin = document.getElementById('form-control');
+    const generatedPin = generatePin.value;
+
+    const fromControl = document.getElementById('from-control');
+    const controlFrom = fromControl.value;
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailureMessage = document.getElementById('pin-failure');
+    if(generatedPin === controlFrom){
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+    }
+    else{
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+    }
 })
